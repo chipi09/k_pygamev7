b5dfc8969cf13b14193853ca9115c58affc33675
Rubén Crespo <Ruben2000@hotmail.es>
2021-04-30 17:05:08 +0200

    primer commit

---
+++ hello.py
@@ -0,0 +1,28 @@
+import pygame as pg
+import sys
+
+def fin_juego():
+    pg.quit()
+    sys.exit()
+
+pg.init()
+pantalla = pg.display.set_mode((600,  400))
+pg.display.set_caption("Hola")
+
+game_over = False
+
+while not game_over:
+    # Gestion de eventos
+    for evento in  pg.event.get():
+        if evento.type == pg.QUIT:
+            game_over = True
+
+    # Gestion del estado
+    print("Hola mundo")
+    
+    # Refrescar pantalla
+    pantalla.fill((0, 255, 0))
+    pg.display.flip()
+
+
+fin_juego()
